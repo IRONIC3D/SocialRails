65ed862e48fe112a4407a2109a04d772f3701e51
Iyad Horani <i3d@ironic3d.com>
2014-06-29 02:22:15 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialist::Application.config.secret_token = 'd702ea356c201fae80411a472ee1509c93d1494862c2270edf4ec754f96e6cf8fbab31f95225320a08657a717c506719fc62ff2a0d0d2cfa46d239400bc4040e'

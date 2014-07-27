8b8b91ea2d483272c4cb73f02ed81f1fc9123290
Iyad Horani <i3d@ironic3d.com>
2014-07-27 04:30:47 +0000

    Installing devise

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Socialist
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Heroku specific Devise configuration
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Socialist::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Devise specific configuration
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end

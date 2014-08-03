cfac4af6a5ea0dda126cae03ba3c09c64a2ad5fc
Iyad Horani <i3d@ironic3d.com>
2014-08-03 13:06:39 +0000

    trying all fixes p3

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,6 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
-    # Heroku specific Devise configuration
-    config.assets.initialize_on_precompile = false
-
 module Socialist
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -62,5 +59,8 @@ module Socialist
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Heroku specific Devise configuration
+    config.assets.initialize_on_precompile = false
   end
 end

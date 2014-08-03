44bef13a937df0722edae902b2693bdced89ad62
Iyad Horani <i3d@ironic3d.com>
2014-08-03 03:39:23 +0000

    Create custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Socialist::Application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get "register", to: "devise/registrations#new", as: :register
+    get "login", to: "devise/sessions#new", as: :login
+    get "logout", to: "devise/sessions#destroy", as: :logout
+  end
 
   resources :statuses
+  get "updates", to: "statuses#index", as: :updates
 
   root to: "statuses#index"
-
 end
